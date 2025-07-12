dc36a566e788975a2e5f6878e4aa081fca9da2eb
GDOCCHI <dkdltlch@gmail.com>
2025-07-11 22:46:31 -0400

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// main.cpp
+#include <stdio.h>
+#include "sum.h"
+
+int main() {
+	char tarck[] = "취약점";
+	char name[] = "조해성";
+	printf("[bob14][%s]sum-test[%s]", track, name);
+	int s = sum(10);
+	printf("result=%d\n", s);
+}
